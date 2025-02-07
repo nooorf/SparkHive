2d089c27a84a9a621a6de94ff6741f52d00fc70d
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-02-08 01:51:44 +0500

    Initial set up including github OAuth, tailwind config, and Navbar

---
+++ README.md
@@ -0,0 +1,37 @@
+🚀 Spark Hive – Showcase Your Tech Projects
+
+🔥 About the Project
+
+Spark Hive is a platform exclusively for tech enthusiasts to upload, showcase, and discover software development projects. With GitHub authentication, users can seamlessly connect their repositories, share their work, and engage with a community of like-minded developers.
+
+✨ Features
+
+🚀 Upload Tech Projects – Showcase your coding skills and innovations
+🔍 Explore & Discover – Browse projects from fellow developers
+🔗 GitHub Integration – Authenticate via GitHub and link repositories
+🏆 Build Your Developer Portfolio – Display projects that stand out
+
+🛠️ Tech Stack
+
+Next.js
+GitHub OAuth
+Sanity.io
+Netlify
+
+👾 Getting Started
+
+Prerequisites
+Ensure you have the following installed:
+
+Node.js & npm/yarn
+GitHub account for authentication
+
+git clone https://github.com/nooorf/Spark-Hive
+cd Spark-Hive
+npm install
+npm run dev
+
+⚠️ Project Status: Under Development 🚧
+
+This project is incomplete and currently in active development.
+
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { auth, signIn, signOut } from '@/auth'
+
+const Navbar = async () => { //async function only on server side cannot be in "use client"
+    const session = await auth()
+    console.log(session)
+  return (
+    <header className="px-5 py-3 shadow-sm bg-white font-work-sans">
+        <nav className='flex justify-between items-center'>
+            <Link href='/'>
+                <Image src='/logo.png' width={144} height={40} alt='logo' />
+            </Link>
+            <div className='flex items-center gap-5 text-black'>
+                {session && session.user ? ( //check if we have an active session and user id from that session
+                    <>
+                        <Link href='/startup/create'>
+                        <span>Create</span>
+                        </Link>
+
+                        <form action={async()=>{ /* We use React 19's <form> instead of a simple <button> because 
+                                                    the sign-in function is asynchronous. Since <button> is a 
+                                                    client-side element, it does not support an async function 
+                                                    directly in the onClick property. */
+
+
+                            "use server"
+                            await signOut({redirectTo: '/'}) //redirectTo is a property of signOut function
+                        }}>
+                            <button type='submit'><span>Logout</span></button>
+                        </form>
+
+                        <Link href={`/users/${session.user?.id}`}>
+                            <span>{session?.user?.name}</span>
+                        </Link>
+                    </>
+                    
+                ) : (
+                    <form action={async()=>{ 
+                        "use server"
+                        await signIn()
+                    }}>
+                       <button type='submit' ><span>Login</span></button> 
+                    </form>
+                )}
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
